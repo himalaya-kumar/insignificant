0c28c70b3a7ff4fd0dfa64875aa340e0a88c1988
Himalaya Kumar <itshimalaykumarx@gmail.com>
2021-10-10 20:25:57 +0530

    Commit

--- src/main/java/com/ai/CnnModel.java
+++ src/main/java/com/ai/CnnModel.java
@@ -5,10 +5,12 @@ import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 public class CnnModel {
 
@@ -25,7 +27,9 @@ public class CnnModel {
 		MultiLayerConfiguration configuration = new NeuralNetConfiguration.Builder().seed(1611)
 				.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
 				.learningRate(properties.getLearningRate()).regularization(Boolean.TRUE)
-				.updater(properties.getOptimizer()).list().layer(0, convo5x5()).pretrain(false).backprop(Boolean.TRUE)
+				.updater(properties.getOptimizer()).list().layer(0, convo5x5()).layer(1, pooling2x2Stride2())
+				.layer(2, convo3x3Stride1Padding2()).layer(3, pooling2x2Stride1()).layer(4, convo3x3Stride1Padding1())
+				.layer(5, pooling2x2Stride1()).layer(6, dense()).pretrain(false).backprop(Boolean.TRUE)
 				.setInputType(dataSetService.inputType()).build();
 		network = new MultiLayerNetwork(configuration);
 	}
@@ -41,33 +45,35 @@ public class CnnModel {
 	 * @return
 	 */
 	private ConvolutionLayer convo5x5() {
-		return new ConvolutionLayer.Builder(5, 5)
-				.nIn(3).nOut(16).stride(1, 1).padding(1, 1)
-				.weightInit(WeightInit.XAVIER_UNIFORM)
-				.activation(Activation.RELU).build();
+		return new ConvolutionLayer.Builder(5, 5).nIn(3).nOut(16).stride(1, 1).padding(1, 1)
+				.weightInit(WeightInit.XAVIER_UNIFORM).activation(Activation.RELU).build();
 	}
 
 	public Evaluation evaluate() {
 		return network.evaluate(dataSetService.testIterator());
 	}
 
-	private SubsamplingLayer pooling2x2Stride() {
-		return new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
-				.kernelSize(2, 2).stride(2, 2).build();
+	private SubsamplingLayer pooling2x2Stride2() {
+		return new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX).kernelSize(2, 2).stride(2, 2).build();
 	}
-	
+
 	private ConvolutionLayer convo3x3Stride1Padding2() {
-		return new ConvolutionLayer.Builder(3,3)
-				.nOut(32).stride(1,1).padding(2,2)
-				.weightInit(WeightInit.XAVIER_UNIFORM)
-				.activation(Activation.RELU).build();
+		return new ConvolutionLayer.Builder(3, 3).nOut(32).stride(1, 1).padding(2, 2)
+				.weightInit(WeightInit.XAVIER_UNIFORM).activation(Activation.RELU).build();
 	}
-	
+
 	private SubsamplingLayer pooling2x2Stride1() {
-		return new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
-				.kernelSize(2,2)
-				.stride(1,1)
-				.build();
+		return new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX).kernelSize(2, 2).stride(1, 1).build();
 	}
-	
+
+	private ConvolutionLayer convo3x3Stride1Padding1() {
+		return new ConvolutionLayer.Builder(3, 3).nOut(64).stride(1, 1).padding(1, 1)
+				.weightInit(WeightInit.XAVIER_UNIFORM).activation(Activation.RELU).build();
+	}
+
+	private OutputLayer dense() {
+		return new OutputLayer.Builder(LossFunctions.LossFunction.MEAN_SQUARED_LOGARITHMIC_ERROR)
+				.activation(Activation.SOFTMAX).weightInit(WeightInit.XAVIER_UNIFORM)
+				.nOut(dataSetService.labels().size() - 1).build();
+	}
 }
