31f675e408e2320a3bfb3bb8238260a1540b9ef9
Himalaya Kumar <itshimalaykumarx@gmail.com>
2021-12-18 12:39:13 +0530

    Problem 06 : Verify a palindrome

--- src/main/java/com/ai/Problem_06.java
+++ src/main/java/com/ai/Problem_06.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Find out if list is an palindrome
@@ -12,16 +14,29 @@ public class Problem_06 {
 
     /**
      * Is An Palindrome list
+     *
      * @param <T>
      * @return
      */
     public static <T> boolean isPalindrome(List<T> list) {
         List<T> compareList = new ArrayList<>(list);
         Collections.reverse(list);
-        return Objects.equals(list,compareList);
+        return Objects.equals(list, compareList);
     }
-//
-//    public static <T> boolean isPalindrome_IntStream(){
-//
-//    }
+
+    /**
+     * Palindrome using int stream
+     *
+     * @param list
+     * @param <T>
+     * @return
+     */
+    public static <T> boolean isPalindrome_IntStream(List<T> list) {
+        if (list == null) {
+            throw new IllegalArgumentException("List can't not be null");
+        }
+        int size = list.size();
+        List<T> reversedList = IntStream.iterate(size - 1, el -> el - 1).limit(size).mapToObj(list::get).collect(Collectors.toList());
+        return Objects.equals(list, reversedList);
+    }
 }
--- src/test/java/com/ai/Problem_06_Tests.java
+++ src/test/java/com/ai/Problem_06_Tests.java
@@ -1,4 +1,43 @@
 package com.ai;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+@DisplayName("SUITE_6")
 public class Problem_06_Tests {
+
+    @Test
+    @DisplayName("TEST_1:Verify Palindrome using collection")
+    public void verify_palindrome_using_collection() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 2, 1);
+        Assert.assertThat(Problem_06.isPalindrome(list), is(equalTo(Boolean.TRUE)));
+    }
+
+    @Test
+    @DisplayName("TEST_2:Verify Palindrome using collection")
+    public void verify_not_palindrome_using_collection() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        Assert.assertThat(Problem_06.isPalindrome(list), is(equalTo(Boolean.FALSE)));
+    }
+
+    @Test
+    @DisplayName("TEST_3:Verify Palindrome using int stream collection")
+    public void verify_palindrome_using_intStream() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 2, 1);
+        Assert.assertThat(Problem_06.isPalindrome_IntStream(list), is(equalTo(Boolean.TRUE)));
+    }
+
+    @Test
+    @DisplayName("TEST_4:Verify not Palindrome using int stream")
+    public void verify_not_palindrome_using_intStream() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        Assert.assertThat(Problem_06.isPalindrome_IntStream(list), is(equalTo(Boolean.FALSE)));
+    }
 }
