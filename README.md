7f16c9d03304756a5859579d8782a872e7b84460
Himalaya Kumar <itshimalaykumarx@gmail.com>
2021-12-19 20:48:08 +0530

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # insignificant
 An insignificant project
+mvn clean install -Dmaven.test.skip=true
+mvn surefire:test
--- src/test/java/com/ai/Problem_02_Tests.java
+++ src/test/java/com/ai/Problem_02_Tests.java
@@ -7,14 +7,13 @@ import org.junit.jupiter.api.Test;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static com.ai.UtilityClass.linkedList;
 import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 /**
  * TESTS for P02
@@ -24,26 +23,26 @@ public class Problem_02_Tests {
     @Test
     @DisplayName("TEST_1:Get Last Element")
     public void getLastElement_List_way_Test() {
-        assertThat(Problem_02.secondLast(asList("a","b","c","d","e")),is(equalTo("d")));
+        assertThat(Problem_02.secondLast(asList("a", "b", "c", "d", "e")), is(equalTo("d")));
     }
 
     @Test
     @DisplayName("TEST_2:Last Element in Linked List")
     public void getLastElement_Linked_List_way_Test() {
-        LinkedList<String> list = linkedList("a","b","c","d","e");
-        assertThat(Problem_02.secondLastRecursion(list),is(equalTo("d")));
+        LinkedList<String> list = linkedList("a", "b", "c", "d", "e");
+        assertThat(Problem_02.secondLastRecursion(list), is(equalTo("d")));
     }
 
     @Test
     @DisplayName("TEST_3:The Recursive Way")
-    public void recursion_is_a_way(){
-        assertThat(Problem_02.secondLastRecursion(linkedList("a","b","c","d","e")),is(equalTo("d")));
+    public void recursion_is_a_way() {
+        assertThat(Problem_02.secondLastRecursion(linkedList("a", "b", "c", "d", "e")), is(equalTo("d")));
     }
 
     @Test
     @DisplayName("TEST_4:Exception Verification")
-    public void noSuchElementExceptionTest(){
-        Assertions.assertThrows(NoSuchElementException.class,() -> {
+    public void noSuchElementExceptionTest() {
+        Assertions.assertThrows(NoSuchElementException.class, () -> {
             Problem_02.secondLast(Collections.EMPTY_LIST);
         });
     }
